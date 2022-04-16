21b4118d18d8040019401009542682b75b341a16
Jviguy <jviguytwo2@gmail.com>
2022-04-15 23:34:06 -0400

    start rewrite

--- plugins/test/main.js
+++
@@ -1,7 +0,0 @@
-//checked after loading to decide whether we should change the handler...
-logger.Info("Testing Plugin Load started...");
-if (sun.Handler() === sun.Handler()) {
-    logger.Info("YES")
-} else {
-    logger.Info("NO")
-}
